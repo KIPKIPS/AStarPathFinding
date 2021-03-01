171820d46231642b9bbfb92e31fcedb092baea18
KIPKIPS <2432719656@qq.com>
2021-03-02 01:26:01 +0800

    完善网格节点类

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -1,7 +1,15 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Grid {
-
+public class Grid : MonoBehaviour
+{
+    public Vector2 gridWorldSize;
+    public float nodeRadius;
+    Node[,] grid;
+    public LayerMask unwalkableMask;
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
+    }
 }
