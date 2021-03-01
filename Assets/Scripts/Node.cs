f8b0762395651ff2dd7a2c31341167411f375779
KIPKIPS <2432719656@qq.com>
2021-03-02 01:50:30 +0800

    环境搭建

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //网格节点类
-public class Node {
+public class Node
+{
     public bool walkable;//该网格节点是否可以行走
     public Vector3 worldPosition;//节点的世界坐标
 
     //构造函数
-    public Node(bool _walkable, Vector3 _worldPosition) {
+    public Node(bool _walkable, Vector3 _worldPosition)
+    {
         walkable = _walkable;
         worldPosition = _worldPosition;
     }
+
 }
