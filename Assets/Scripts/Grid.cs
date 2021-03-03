286b8d8bbf917de5cb79e27d85dc648b7f81fe87
KIPKIPS <2432719656@qq.com>
2021-03-04 01:48:59 +0800

    计算障碍物

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -1,3 +1,4 @@
+using System.Drawing;
 using System.Net.Mime;
 using System.Collections;
 using System.Collections.Generic;
@@ -14,6 +15,14 @@ public class Grid : MonoBehaviour
     void OnDrawGizmos()
     { //用来显示一个三维向量的包围框
         Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
+        if (grid != null)
+        {
+            foreach (Node n in grid)
+            {
+                Gizmos.color = n.walkable ? Color.white : Color.red;
+                Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter - 0.1f));
+            }
+        }
     }
     void Start()
     {
@@ -25,11 +34,14 @@ public class Grid : MonoBehaviour
     void CreatGrid()
     {
         grid = new Node[gridSizeX, gridSizeY];//创建一个Node节点二维数组
+        Vector3 worldBttomLeft = transform.position + Vector3.left * gridWorldSize.x / 2 + Vector3.back * gridWorldSize.y / 2;//网格的左下角坐标
         for (int x = 0; x < gridSizeX; x++)
         {
             for (int y = 0; y < gridSizeY; y++)
             {
-
+                Vector3 worldPoint = worldBttomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);//计算出每一个节点的坐标
+                bool walkable = !(Physics.CheckSphere(worldPoint, nodeRadius, unwalkableMask));//定义的球体是否和物体相撞,处于layerMask的物体
+                grid[x, y] = new Node(walkable, worldPoint);
             }
         }
     }
