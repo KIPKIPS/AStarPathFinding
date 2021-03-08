a8edb77c81ed75e988a3714f74145de6f982efcc
KIPKIPS <2432719656@qq.com>
2021-03-09 02:48:03 +0800

    寻路算法编写
    开列表List 关列表HashSet

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -8,11 +8,21 @@ public class Node
     public bool walkable;//该网格节点是否可以行走
     public Vector3 worldPosition;//节点的世界坐标
 
+    public int gCost;//G
+    public int hCost;//H
+
     //构造函数
     public Node(bool _walkable, Vector3 _worldPosition)
     {
         walkable = _walkable;
         worldPosition = _worldPosition;
+    }
+    public int fCost
+    { //F属性字段
+        get
+        {
+            return hCost + gCost;
+        }
     }
 
 }
---
+++ Assets/Scripts/PathFinding.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFinding : MonoBehaviour
+{
+
+    Grid grid;
+    void Awake()
+    {
+        grid = GetComponent<Grid>();
+    }
+    void FindPath(Vector3 startPos, Vector3 targetPos)
+    {
+        Node startNode = grid.NodeFromWorldPoint(startPos);//获取开始节点
+        Node targetNode = grid.NodeFromWorldPoint(targetPos);//获取目标节点
+
+        List<Node> openSet = new List<Node>();
+        HashSet<Node> closedSet = new HashSet<Node>();
+
+        openSet.Add(startNode);//起始节点添加到开放列表
+        while (openSet.Count > 0)
+        {
+
+        }
+    }
+}
