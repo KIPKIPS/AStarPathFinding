dac38f48feb19e6f340183735e60d9e14bc0bb7c
KIPKIPS <2432719656@qq.com>
2021-03-27 02:32:30 +0800

    实现小根堆

---
+++ Assets/Scripts/Heap.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+//使用泛型类,不光可以处理节点的排序,还可以处理其他类型的数据
+//小根堆
+public class Heap<T> where T : IHeapItem<T> {
+    private T[] items;
+    private int curItemCount;
+
+    public Heap(int maxHeapSize) {
+        items = new T[maxHeapSize];//创建一个数组用来存储堆
+    }
+
+    public int Count {
+        get {
+            return curItemCount;
+        }
+    }
+
+    public void UpdateItem(T item) {
+        SortUp(item);
+    }
+
+    public void Add(T item) {
+        item.HeapIndex = curItemCount;
+        items[curItemCount] = item;
+        SortUp(item);
+        curItemCount++;
+    }
+
+    public void SortUp(T item) {
+        int parentIndex = (item.HeapIndex - 1) / 2;
+        while (true) {
+            T parentItem = items[parentIndex];
+            if (item.Compare(item, parentItem) > 0) {
+                Swap(item, parentItem);
+            } else {
+                break;
+            }
+
+            parentIndex = (item.HeapIndex - 1) / 2;//父节点索引
+        }
+    }
+    public T RemoveFirst() { //移除堆顶元素,并返回
+        T firstItem = items[0];
+        curItemCount--;
+        items[0] = items[curItemCount];//堆顶元素和堆底元素交换
+        items[0].HeapIndex = 0;//堆顶元素索引置为0,更新索引
+        SortDown(items[0]);
+        return firstItem;
+    }
+
+    public void SortDown(T item) {//自顶向下
+        while (true) {
+            int childLeftIndex = item.HeapIndex * 2 + 1;
+            int childRightIndex = item.HeapIndex * 2 + 2;
+            //寻找最小值的节点索引
+            int swapIndex = 0;
+            if (childLeftIndex < curItemCount) {
+                swapIndex = childLeftIndex;//初始化
+                if (childRightIndex < curItemCount) {
+                    //Compare
+                    if (items[childLeftIndex].Compare(items[childLeftIndex], items[childRightIndex]) < 0) {
+                        swapIndex = childRightIndex;
+                    }
+                }
+
+                if (item.Compare(item, items[swapIndex]) < 0) {
+                    Swap(item, items[swapIndex]);
+                } else {
+                    return;
+                }
+            } else {
+                return;
+            }
+
+        }
+    }
+
+    public bool Contains(T item) {
+        return Equals(items[item.HeapIndex], item);
+    }
+
+    void Swap(T itemA, T itemB) {
+        //引用类型交换赋值
+        items[itemA.HeapIndex] = itemB;
+        items[itemB.HeapIndex] = itemA;
+
+        //值类型的借助临时变量交换
+        int itemAIndex = itemA.HeapIndex;
+        itemA.HeapIndex = itemB.HeapIndex;
+        itemB.HeapIndex = itemAIndex;
+    }
+}
+
+public interface IHeapItem<T> : IComparer<T> {
+    int HeapIndex { get; set; }
+}
--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //网格节点类
-public class Node
-{
+public class Node : IHeapItem<Node> {
     public bool walkable;//该网格节点是否可以行走
     public Vector3 worldPosition;//节点的世界坐标
 
@@ -16,21 +15,42 @@ public class Node
 
     public Node parent;//父节点
     public bool existedInOpenset = false;
+    private int heapIndex;
 
     //构造函数
-    public Node(bool _walkable, Vector3 _worldPosition, int _gridX, int _gridY)
-    {
+    public Node(bool _walkable, Vector3 _worldPosition, int _gridX, int _gridY) {
         walkable = _walkable;
         worldPosition = _worldPosition;
         gridX = _gridX;
         gridY = _gridY;
     }
-    public int fCost
-    { //F属性字段
-        get
-        {
+    public int fCost { //F属性字段
+        get {
             return hCost + gCost;
         }
     }
 
+    public int HeapIndex {
+        get {
+            return heapIndex;
+        }
+        set {
+            heapIndex = value;
+        }
+    }
+
+    public int Compare(Node curNode, Node compareNode) {
+        // int compare = fCost.CompareTo(compareNode.fCost);
+        // if (compare == 0) {
+        //     compare = hCost.CompareTo(compareNode.hCost);
+        // }
+        //
+        // return -compare;
+        int compare = fCost - compareNode.fCost;//F Cost 小于比较的节点
+        if (compare == 0) {
+            compare = hCost - compareNode.hCost; //F Cost相等就比较H Cost
+        }
+
+        return -compare;
+    }
 }
