edf52795329916a9efb719d085a186b8eb6a7241
KIPKIPS <2432719656@qq.com>
2021-03-27 02:32:43 +0800

    优化寻路算法

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -5,8 +5,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Grid : MonoBehaviour
-{
+public class Grid : MonoBehaviour {
+    public bool onlyDisplayPathGizoms;
     public Vector2 gridWorldSize;//节点坐标
     public float nodeRadius;//节点半径
     Node[,] grid;
@@ -14,48 +14,48 @@ public class Grid : MonoBehaviour
     int gridSizeX, gridSizeY;//节点的长宽数量
     float nodeDiameter;//节点直径
     GameObject wallObj;
-    Vector3 worldBttomLeft;//左下角坐标
+    Vector3 worldBottomLeft;//左下角坐标
     public Transform seeker;
     public Transform target;
     public List<Node> path;
     float lastTime = 0f;
-    void Awake()
-    {
+    void Awake() {
         seeker = GameObject.Find("Seeker").transform;
         target = GameObject.Find("Target").transform;
     }
-    void Update()
-    {
+    void Update() {
         lastTime += Time.deltaTime;
     }
-    void OnDrawGizmos()
-    { //用来显示一个三维向量的包围框
+    void OnDrawGizmos() { //用来显示一个三维向量的包围框
         Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
-        if (grid != null)
-        {
-            Node playerNode = NodeFromWorldPoint(seeker.transform.position);
-            Node targetNode = NodeFromWorldPoint(target.transform.position);
-            foreach (Node n in grid)
-            {
-                Gizmos.color = playerNode == n ? Color.cyan : (targetNode == n ? Color.green : (n.walkable ? Color.white : Color.red)); //起始点则置为青色,目标绿色,否则根据是否障碍物进行判断,障碍物红色,通路白色
-                if (path != null)
-                {
-                    if (path.Contains(n) && n != targetNode)
-                    {
-                        Gizmos.color = Color.black;
-                    }
-                    else if (n.existedInOpenset == true)
-                    {
-                        Gizmos.color = Color.magenta;
-                    }
+        if (onlyDisplayPathGizoms) {
+            if (path != null) {
+                foreach (Node n in path) {
+                    Gizmos.color = Color.black;
+                    Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter - 0.1f));//着色
                 }
-                Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter - 0.1f));//着色
             }
-
+        } else {
+            if (grid != null) {
+                Node playerNode = NodeFromWorldPoint(seeker.transform.position);
+                Node targetNode = NodeFromWorldPoint(target.transform.position);
+                foreach (Node n in grid) {
+                    Gizmos.color = playerNode == n ? Color.cyan : (targetNode == n ? Color.green : (n.walkable ? Color.white : Color.red)); //起始点则置为青色,目标绿色,否则根据是否障碍物进行判断,障碍物红色,通路白色
+                    if (path != null) {
+                        if (path.Contains(n) && n != targetNode) {
+                            Gizmos.color = Color.black;
+                        }
+                        //else if (n.existedInOpenset) {
+                        //Gizmos.color = Color.magenta;
+                        //}
+                    }
+                    Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter - 0.1f));//着色
+                }
+            }
         }
+
     }
-    void Start()
-    {
+    void Start() {
         seeker.gameObject.SetActive(true);
         target.gameObject.SetActive(true);
         wallObj = GameObject.Find("Walls");
@@ -65,15 +65,19 @@ public class Grid : MonoBehaviour
         gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
         CreatGrid();
     }
-    void CreatGrid()
-    {
+
+    public int MapSize {
+        get {
+            return gridSizeX * gridSizeY;
+        }
+    }
+    void CreatGrid() {
         grid = new Node[gridSizeX, gridSizeY];//创建一个Node节点二维数组
-        worldBttomLeft = transform.position + Vector3.left * gridWorldSize.x / 2 + Vector3.back * gridWorldSize.y / 2;//网格的左下角坐标
-        for (int x = 0; x < gridSizeX; x++)
-        {
-            for (int y = 0; y < gridSizeY; y++)
-            {
-                Vector3 worldPoint = worldBttomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);//计算出每一个节点的坐标
+        worldBottomLeft = transform.position + Vector3.left * gridWorldSize.x / 2 + Vector3.back * gridWorldSize.y / 2;//网格的左下角坐标
+        for (int x = 0; x < gridSizeX; x++) {
+            for (int y = 0; y < gridSizeY; y++) {
+                //计算出每一个节点的坐标
+                Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
                 bool walkable = !(Physics.CheckSphere(worldPoint, nodeRadius, unwalkableMask));//定义的球体是否和物体相撞,处于layerMask的物体
                 grid[x, y] = new Node(walkable, worldPoint, x, y);
             }
@@ -85,12 +89,10 @@ public class Grid : MonoBehaviour
     }
 
     //根据节点中心坐标返回对应的Node
-    public Node NodeFromWorldPoint(Vector3 worldPosition)
-    {
-        //计算索引
-        int xIndex = Mathf.RoundToInt(worldPosition.x - worldBttomLeft.x - 0.5f);
-        int yIndex = Mathf.RoundToInt(worldPosition.z - worldBttomLeft.z - 0.5f);
-        //print((worldPosition.x - worldBttomLeft.x).ToString() + "  " + (worldPosition.z - worldBttomLeft.z).ToString());
+    public Node NodeFromWorldPoint(Vector3 worldPosition) { //计算索引
+        int xIndex = Mathf.RoundToInt(worldPosition.x - worldBottomLeft.x - 0.5f);
+        int yIndex = Mathf.RoundToInt(worldPosition.z - worldBottomLeft.z - 0.5f);
+        //print((worldPosition.x - worldBottomLeft.x).ToString() + "  " + (worldPosition.z - worldBottomLeft.z).ToString());
         //print("x : " + xIndex + " , y : " + yIndex + "");
         xIndex = xIndex < 0 ? 0 : xIndex >= gridSizeX ? gridSizeX - 1 : xIndex;
         yIndex = yIndex < 0 ? 0 : yIndex >= gridSizeY ? gridSizeY - 1 : yIndex;
@@ -98,23 +100,17 @@ public class Grid : MonoBehaviour
     }
 
     //获取一个节点的邻居节点
-    public List<Node> GetNeighbours(Node node)
-    {
+    public List<Node> GetNeighbours(Node node) {
         List<Node> neighboursList = new List<Node>();
-        if (node.gridX <= grid.GetLength(0) && node.gridY <= grid.GetLength(1))
-        {
-            for (int x = -1; x <= 1; x++)
-            {
-                for (int y = -1; y <= 1; y++)
-                {
-                    if (x == 0 && y == 0)
-                    {
+        if (node.gridX <= grid.GetLength(0) && node.gridY <= grid.GetLength(1)) {
+            for (int x = -1; x <= 1; x++) {
+                for (int y = -1; y <= 1; y++) {
+                    if (x == 0 && y == 0) {
                         continue;
                     }
                     int checkX = node.gridX + x;
                     int checkY = node.gridY + y;
-                    if (checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY)
-                    {
+                    if (checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY) {
                         neighboursList.Add(grid[checkX, checkY]);
                     }
                 }
--- Assets/Scripts/PathFinding.cs
+++ Assets/Scripts/PathFinding.cs
@@ -2,81 +2,80 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Diagnostics;
 
-public class PathFinding : MonoBehaviour
-{
+public class PathFinding : MonoBehaviour {
     public Transform seeker, target;
     Grid grid;
-    void Awake()
-    {
+    void Awake() {
         seeker = GameObject.Find("Seeker").transform;
         target = GameObject.Find("Target").transform;
         grid = GetComponent<Grid>();
     }
-    void Start()
-    {
+    void Start() {
 
     }
-    void Update()
-    {
-        FindPath(seeker.position, target.position);
-    }
-    void FindPath(Vector3 startPos, Vector3 targetPos)
-    {
-        Node startNode = grid.NodeFromWorldPoint(startPos);//获取开始节点
-        Node targetNode = grid.NodeFromWorldPoint(targetPos);//获取目标节点
+    void Update() {
+        if (Input.GetButtonDown("Jump")) {
+            FindPath(seeker.position, target.position);
+        }
 
-        List<Node> openSet = new List<Node>();
+    }
+    void FindPath(Vector3 startPos, Vector3 targetPos) {
+        //定时器
+        Stopwatch sw = new Stopwatch();
+        sw.Start();
+
+        Node startNode = grid.NodeFromWorldPoint(startPos); //获取开始节点
+        Node targetNode = grid.NodeFromWorldPoint(targetPos); //获取目标节点
+
+        //List<Node> openSet = new List<Node>();//
+        //使用堆的数据结构来构造openSet
+        Heap<Node> openSet = new Heap<Node>(grid.MapSize);
         HashSet<Node> closedSet = new HashSet<Node>();
 
-        openSet.Add(startNode);//起始节点添加到开放列表
+        openSet.Add(startNode); //起始节点添加到开放列表
 
-        while (openSet.Count > 0) //开放列表不为空表示存在可以搜索的节点,寻路
-        {
-            Node currentNode = openSet[0];
-            //这里效率太慢,每次都遍历整个openSet来寻找最低消耗的节点,使用堆来优化,维护一个堆数据结构
-            for (int i = 0; i < openSet.Count; i++)
-            {
-                //若查询到F(总消耗)小于当前节点的,或者总消耗相同,但H(剩余耗费)小于当前节点的
-                if (openSet[i].fCost < currentNode.fCost || openSet[i].fCost == currentNode.fCost && openSet[i].hCost < currentNode.hCost)
-                {
-                    currentNode = openSet[i];
-                }
-            }
-            openSet.Remove(currentNode);
+        while (openSet.Count > 0) { //开放列表不为空表示存在可以搜索的节点,寻路
+            // Node currentNode = openSet[0];
+            // //这里效率太慢,每次都遍历整个openSet来寻找最低消耗的节点,使用堆来优化,维护一个堆数据结构
+            // for (int i = 0; i < openSet.Count; i++) {
+            //     //若查询到F(总消耗)小于当前节点的,或者总消耗相同,但H(剩余耗费)小于当前节点的
+            //     if (openSet[i].fCost < currentNode.fCost || openSet[i].fCost == currentNode.fCost && openSet[i].hCost < currentNode.hCost) {
+            //         currentNode = openSet[i];
+            //     }
+            // }
+            // openSet.Remove(currentNode);
+            Node currentNode = openSet.RemoveFirst();//移除堆顶,小根堆
             closedSet.Add(currentNode);
 
-            if (currentNode == targetNode)
-            { //寻找到目标节点
+            if (currentNode == targetNode) { //寻找到目标节点
+                sw.Stop();
+                print("Time : " + sw.ElapsedMilliseconds + "ms");
                 RetracePath(startNode, targetNode);
                 return;
             }
             //将邻居添加到开列表
-            foreach (Node neighbour in grid.GetNeighbours(currentNode))
-            {
-                if (!neighbour.walkable || closedSet.Contains(neighbour)) //该邻居节点是障碍物,或者已经检测过了
-                {
+            foreach (Node neighbour in grid.GetNeighbours(currentNode)) {
+                if (!neighbour.walkable || closedSet.Contains(neighbour)) { //该邻居节点是障碍物,或者已经检测过了
                     continue;
                 }
                 //计算新的G,从当前节点移动到目标邻居节点
                 int newMoveCost = currentNode.gCost + GetDistance(currentNode, neighbour);
-                if (newMoveCost < neighbour.gCost || !openSet.Contains(neighbour)) //已消耗小于
-                {
-                    neighbour.gCost = newMoveCost;//更新gCost
+                if (newMoveCost < neighbour.gCost || !openSet.Contains(neighbour)) { //已消耗小于
+                    neighbour.gCost = newMoveCost; //更新gCost
                     neighbour.hCost = GetDistance(neighbour, targetNode);
                     neighbour.parent = currentNode;
-                    if (!openSet.Contains(neighbour))
-                    {
+                    if (!openSet.Contains(neighbour)) {
                         openSet.Add(neighbour);
-                        neighbour.existedInOpenset = true;//标记是否存在过
+                        neighbour.existedInOpenset = true; //标记是否存在过
                     }
                 }
             }
         }
     }
     //获取节点之间的距离
-    int GetDistance(Node nodeA, Node nodeB)
-    {
+    int GetDistance(Node nodeA, Node nodeB) {
         int disX = Mathf.Abs(nodeA.gridX - nodeB.gridX);
         int disY = Mathf.Abs(nodeA.gridY - nodeB.gridY);
         int min = disX > disY ? disY : disX;
@@ -85,12 +84,10 @@ public class PathFinding : MonoBehaviour
     }
 
     //回溯路径
-    void RetracePath(Node startNode, Node endNode)
-    {
+    void RetracePath(Node startNode, Node endNode) {
         List<Node> path = new List<Node>();
         Node currentNode = endNode;
-        while (currentNode != startNode)
-        {
+        while (currentNode != startNode) {
             path.Add(currentNode);
             currentNode = currentNode.parent;
         }
